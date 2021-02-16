50285f86a5f59421a5631dc502ccb91647407a69
Jeffery Cosby <Jeffery.Cosby@omes.ok.gov>
2021-02-15 21:53:26 -0600

    DI providers

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -17,7 +17,7 @@ export class DashboardComponent implements OnInit {
   mostPopularBook: Book;
 
   constructor(private loggerService: LoggerService, private dataService: DataService) {
-    // this.loggerService.log('Creating the dashboard.');
+    this.loggerService.log('Creating the dashboard.');
   }
 
   ngOnInit() {
