81df88eceb99769e07e5242cb003b41d2a9c1e16
Jeffery Cosby <Jeffery.Cosby@omes.ok.gov>
2021-02-20 22:48:00 -0600

    observables

--- src/app/core/data.service.ts
+++ src/app/core/data.service.ts
@@ -1,18 +1,38 @@
 import { Injectable } from '@angular/core';
 import { allBooks, allReaders } from 'app/data';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
+import { Observable, throwError } from 'rxjs';
+import { catchError } from 'rxjs/operators';
 import { Book } from 'app/models/book';
 import { Reader } from 'app/models/reader';
 import { LoggerService } from './logger.service';
+import { BookTrackerError } from 'app/models/bookTrackerError';
 
 @Injectable()
 export class DataService {
 
   mostPopularBook: Book = allBooks[0];
 
-  constructor(private loggerService: LoggerService) { }
+  constructor(private loggerService: LoggerService,
+    private http: HttpClient) { }
 
-  getAllReaders(): Reader[] {
-    return allReaders;
+  getAllReaders(): Observable<Reader[] | BookTrackerError> {
+    return this.http.get<Reader[]>('/api/readers')
+    .pipe(
+      catchError(this.handleError)
+    );
+    // return this.http.get<Reader[]>('/api/errors/500')
+    // .pipe(
+    //   catchError(this.handleError)
+    // );
+  }
+
+  private handleError(error: HttpErrorResponse): Observable<BookTrackerError> {
+    const dataError = new BookTrackerError();
+    dataError.errorNumber = 100;
+    dataError.message = error.statusText;
+    dataError.friendlyMessage = 'An error occurred retrieving data.';
+    return throwError(dataError);
   }
 
   getReaderById(id: number): Reader {
--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -4,6 +4,9 @@ import { Book } from 'app/models/book';
 import { Reader } from 'app/models/reader';
 import { LoggerService } from 'app/core/logger.service';
 import { DataService } from 'app/core/data.service';
+import { Observable } from 'rxjs';
+import { BooktrackerPage } from '../../../e2e/app.po';
+import { BookTrackerError } from 'app/models/bookTrackerError';
 
 @Component({
   selector: 'app-dashboard',
@@ -22,8 +25,14 @@ export class DashboardComponent implements OnInit {
 
   ngOnInit() {
     this.allBooks = this.dataService.getAllBooks();
-    this.allReaders = this.dataService.getAllReaders();
+    this.dataService.getAllReaders()
+    .subscribe(
+      (data: Reader[]) => this.allReaders = data,
+      (err: BookTrackerError) => console.log(err.friendlyMessage),
+      () => this.loggerService.log('All done getting readers!')
+    );
     this.mostPopularBook = this.dataService.mostPopularBook;
+    this.loggerService.log('Done with dashboard initialization');
   }
 
   deleteBook(bookID: number): void {
